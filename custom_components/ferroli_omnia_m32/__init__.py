037f183ea676d770b9105ccd3f8bc7e9f90d392c
@crug80 <claudio@cr-tech.it>
2024-02-19 13:14:38 +0000

    Initial setup - test 1

--- custom_components/ferroli_omnia_m32/__init__.py
+++ custom_components/ferroli_omnia_m32/__init__.py
@@ -1,7 +1,7 @@
 """Custom integration to integrate integration_blueprint with Home Assistant.
 
 For more details about this integration, please refer to
-https://github.com/ludeeus/integration_blueprint
+https://github.com/crug80/ferroli_OmniaM3.2
 """
 from __future__ import annotations
 
--- custom_components/ferroli_omnia_m32/const.py
+++ custom_components/ferroli_omnia_m32/const.py
@@ -3,7 +3,7 @@ from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
-VERSION = "0.0.0"
+NAME = "Ferroli Omnia M 3.2"
+DOMAIN = "ferroli_omnia_m32"
+VERSION = "0.0.1"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
